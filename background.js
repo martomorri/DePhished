78f67ea20b9b91829ce494973405e606bd6d7963
Martin Morrison <martomorr@gmail.com>
2023-05-15 21:13:17 -0300

    solucionado tema obtener cada mail, no podemos acceder a la url del mail en especifico

--- background.js
+++ background.js
@@ -1,53 +1,43 @@
 // background.js
-// background.js
 
 // Function to fetch HTML and extract URLs
 function processEmailContent(emailId) {
   const urlRegex = /(https?:\/\/[^\s]+)/g;
   console.log(emailId);
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs && tabs.length > 0) {
+      const url = tabs[0].url;
+      console.log(url);
+      fetch(url)
+        .then((response) => response.text())
+        .then((html) => {
+          const matches = html.match(urlRegex);
+          const urls = matches ? [...new Set(matches)] : [];
 
-  // Fetch the email content
-  fetch(`https://mail.google.com/mail/u/0/#inbox/${emailId}`)
-    .then(response => response.text())
-    .then(html => {
-      const matches = html.match(urlRegex);
-      const urls = matches ? [...new Set(matches)] : [];
-
-      console.log(urls);
-    })
-    .catch(error => {
-      console.error(error);
-    });
-}
-
-// Event listener for email click
-function handleEmailClick(tab) {
-  const emailId = tab.dataset.emailId;
-  processEmailContent(emailId);
+          console.log(urls);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  });
 }
 
 // Add a listener for clicks on Gmail emails
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete' && tab.url.includes('mail.google.com')) {
-    console.log(tab);
-    chrome.tabs.executeScript(tabId, { file: 'contentScript.js' });
+  if (changeInfo.status === "complete" && tab.url.includes("mail.google.com")) {
+    console.log(tabId);
+    chrome.scripting.executeScript({
+      target: { tabId: tabId },
+      files: ["./contentScript.js"],
+    });
   }
 });
 
 // Message listener to receive email ID from content script
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(request);
   if (request.emailId) {
-    handleEmailClick(request.emailId);
+    processEmailContent(request.emailId);
   }
-});
-
-// Add an event listener to handle initial page load
-chrome.runtime.onInstalled.addListener(function () {
-    console.log("hola");
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    const tab = tabs[0];
-    if (tab.url.includes('mail.google.com')) {
-      chrome.tabs.executeScript(tab.id, { file: 'contentScript.js' });
-    }
-  });
 });
--- contentScript.js
+++ contentScript.js
@@ -1,22 +1,37 @@
 // contentScript.js
+// console.log("hola");
 
 // Function to get the email ID from the Gmail email page
-function getEmailId(element) {
-    const emailId = element.dataset.emailId;
-    chrome.runtime.sendMessage({ emailId: emailId });
-  }
-  
-  // Add a listener for click events on the email element
-  document.addEventListener('click', function (event) {
-    const emailElement = event.target.closest('.email-class'); // Replace with the appropriate selector to target the email element
-    if (emailElement) {
-      getEmailId(emailElement);
-    }
+// function getEmailId(element) {
+//   // const emailId = element.dataset.emailId;
+//   // console.log(element);
+//   console.log(element.getAttribute("id"));
+//   chrome.runtime.sendMessage({ emailId: element.id });
+// }
+
+function setupMutationObserver() {
+  const observer = new MutationObserver(() => {
+    onDOMLoaded();
   });
-  
-  // Call the getEmailId function to retrieve the email ID if needed on page load
-  const emailElementOnLoad = document.querySelector('.email-class'); // Replace with the appropriate selector to target the email element
-  if (emailElementOnLoad) {
-    getEmailId(emailElementOnLoad);
-  }
-  
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+}
+
+function onDOMLoaded () {
+  const emailElementOnLoad = document.querySelectorAll(
+    'tr[jscontroller="ZdOxDb"]'
+  );
+  console.log(emailElementOnLoad);
+  emailElementOnLoad.forEach((element) => {
+    // Add a listener for click events on the email element
+    // console.log(element);
+    element.addEventListener("click", function (event) {
+      chrome.runtime.sendMessage({ emailId: element.id });
+    });
+  });
+}
+
+onDOMLoaded();
