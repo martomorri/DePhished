a963b577e416a508a10237883dbed5d622f1c08a
martomorri <106319796+martomorri@users.noreply.github.com>
2023-06-12 11:57:23 -0300

    odio js

--- oauth.js
+++ oauth.js
@@ -18,12 +18,21 @@ window.onload = function () {
                 .then(function (data) {
                     console.log(data.messages)
                     data.messages.forEach(element => {
+                        console.log(element.id)
                         fetch(
                             `https://gmail.googleapis.com/gmail/v1/users/me/messages/${element.id}`,
                             init)
                             .then((response) => response.json())
                             .then(function (data) {
-                                console.log(decodeURIComponent(atob(data.payload.parts[0].body.data)))
+                                let mailBody;
+                                let mailHtml;
+                                console.log(data);
+                                if (data.payload.body.size === 0) { mailBody = data.payload.parts[0].body.data; mailHtml =  data.payload.parts[1].body.data; }
+                                else {
+                                    mailBody = data.payload.body;
+                                }
+                                console.log("Text/Plain: " + b64DecodeUnicode(mailBody));
+                                console.log("Html: " + b64DecodeUnicode(mailHtml));
                             });
                     });
                 });
@@ -37,3 +46,19 @@ window.onload = function () {
         });
     });
 };
+
+function b64DecodeUnicode(str) {
+    // Going backwards: from bytestream, to percent-encoding, to original string.
+    // ESTO ES HORRIBLE ARREGLAR CON GURU DE JS
+    let decoded = "";
+    try{
+        decoded = decodeURIComponent(atob(str).split('').map(function(c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''))
+    }catch(DOMException){
+    }
+
+    
+    return decoded;
+
+}
