60f997b5f9c88251c318b2ed369cf0db013fdf8b
martomorri <106319796+martomorri@users.noreply.github.com>
2023-07-03 10:13:53 -0300

    funca la extension, tenemos las url AMEN

--- background.js
+++ background.js
@@ -11,55 +11,30 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
                 },
                 'contentType': 'json'
             };
-            fetch(
-                'https://gmail.googleapis.com/gmail/v1/users/me/messages/',
-                init)
-                .then((response) => response.json())
-                .then(function (data) {
-                    console.log(data.messages)
-                    data.messages.forEach(element => {
-                        console.log(element.id)
-                        fetch(
-                            `https://gmail.googleapis.com/gmail/v1/users/me/messages/${element.id}`,
-                            init)
-                            .then((response) => response.json())
-                            .then(function (data) {
-                                let mailBody;
-                                let mailHtml;
-                                console.log(data);
-                                if (data.payload.body.size === 0) { mailBody = data.payload.parts[0].body.data; mailHtml = data.payload.parts[1].body.data; }
-                                else {
-                                    mailBody = data.payload.body;
-                                }
-                                // console.log("Text/Plain: " + b64DecodeUnicode(mailBody));
-                                // console.log("Html: " + b64DecodeUnicode(mailHtml));
-                                const emailContent = {
-                                    body: b64DecodeUnicode(mailBody),
-                                    html: b64DecodeUnicode(mailHtml)
-                                }
-                                extractUrlsFromEmail(emailContent);
-                            });
-                    });
-                });
             chrome.runtime.sendMessage({ action: 'authenticationComplete', data: token });
             chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
                 if (message.action === 'email-click') {
                     console.log(message.emailId);
-                    // fetch(
-                    //     `https://gmail.googleapis.com/gmail/v1/users/me/messages/${message.emailId}`,
-                    //     init)
-                    //     .then((response) => response.json())
-                    //     .then(function (data) {
-                    //         let mailBody;
-                    //         let mailHtml;
-                    //         console.log(data);
-                    //         if (data.payload.body.size === 0) { mailBody = data.payload.parts[0].body.data; mailHtml = data.payload.parts[1].body.data; }
-                    //         else {
-                    //             mailBody = data.payload.body;
-                    //         }
-                    //         console.log("Text/Plain: " + b64DecodeUnicode(mailBody));
-                    //         console.log("Html: " + b64DecodeUnicode(mailHtml));
-                    //     });
+                    fetch(
+                        `https://gmail.googleapis.com/gmail/v1/users/me/messages/${message.emailId}`,
+                        init)
+                        .then((response) => response.json())
+                        .then(function (data) {
+                            let mailBody;
+                            let mailHtml;
+                            console.log(data);
+                            if (data.payload.body.size === 0) { mailBody = data.payload.parts[0].body.data; mailHtml = data.payload.parts[1].body.data; }
+                            else {
+                                mailBody = data.payload.body;
+                            }
+                            // console.log("Text/Plain: " + b64DecodeUnicode(mailBody));
+                            // console.log("Html: " + b64DecodeUnicode(mailHtml));
+                            const emailContent = {
+                                body: b64DecodeUnicode(mailBody),
+                                html: b64DecodeUnicode(mailHtml)
+                            }
+                            extractUrlsFromEmail(emailContent);
+                        });
                 }
             });
         });
--- contentScript.js
+++ contentScript.js
@@ -1,25 +1,17 @@
-function setupMutationObserver() {
-  const observer = new MutationObserver(() => {
-    onDOMLoaded();
-  });
-
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true
-  });
-}
-
-function onDOMLoaded () {
-  const emailElementOnLoad = document.querySelectorAll(
-    'tr[jscontroller="ZdOxDb"]'
-  );
-  console.log(emailElementOnLoad);
-  emailElementOnLoad.forEach((element) => {
-    console.log(element);
-    element.addEventListener("click", function (event) {
-      chrome.runtime.sendMessage({ action:"email-click", emailId: element.id });
+window.onload = function () {
+    const emailElementOnLoad = document.querySelectorAll(
+        'tr[jscontroller="ZdOxDb"]'
+    );
+    // console.log(emailElementOnLoad);
+    emailElementOnLoad.forEach((element) => {
+        let email_element = element.childNodes[4].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0];
+        let email_id = email_element.getAttribute("data-legacy-last-message-id");
+        if (!email_id) {
+            email_id = email_element.childNodes[0].getAttribute("data-legacy-last-message-id");
+        }
+        console.log(email_id);
+        element.addEventListener("click", function (event) {
+            chrome.runtime.sendMessage({ action: "email-click", emailId: email_id });
+        });
     });
-  });
-}
-
-onDOMLoaded();
+};
