c8f66e841da0aa17eaf83eb00e13dcaf2ab352c1
ALEXIS BURSZTYN <46878021@ort>
2023-07-06 09:01:45 -0300

    vt request (no funciona, esta caido)

--- background.js
+++ background.js
@@ -22,12 +22,12 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
                         .then(function (data) {
                             let mailBody = "";
                             let mailHtml = "";
-                            console.log(data);
+                            // console.log(data);
                             if (data.payload.body.size === 0) {
                                 mailBody = data.payload.parts[0].body.data;
                                 if (data.payload.parts.length > 1) mailHtml = data.payload.parts[1].body.data;
                                 if (mailBody === undefined) {
-                                    console.log(data.payload.parts[0].parts[0].body.data);
+                                    // console.log(data.payload.parts[0].parts[0].body.data);
                                     mailBody = data.payload.parts[0].parts[0].body.data;
                                 }
                             }
@@ -36,12 +36,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
                             }
                             // console.log("Text/Plain: " + b64DecodeUnicode(mailBody));
                             // console.log("Html: " + b64DecodeUnicode(mailHtml));
-                            console.log(mailBody);
+                            // console.log(mailBody);
                             const emailContent = {
                                 body: b64DecodeUnicode(mailBody),
                                 html: b64DecodeUnicode(mailHtml)
                             }
-                            extractUrlsFromEmail(emailContent);
+                            let urls = extractUrlsFromEmail(emailContent);
+                            urls.forEach(url => {
+                                virusTotalRequest(btoa(url));
+                            });
                         });
                 }
             });
@@ -50,7 +53,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 });
 
 function b64DecodeUnicode(str) {
-    console.log(str);
+    // console.log(str);
     return atob(str.replace(/-/g, '+').replace(/_/g, '/'));
 }
 
@@ -59,4 +62,24 @@ function extractUrlsFromEmail(content) {
     const urls = content.body.match(urlRegex) || content.html.match(urlRegex) || [];
     console.log(urls);
     return urls;
-}  
+}
+
+function virusTotalRequest(urlEncoded) {
+    console.log(urlEncoded);
+    const options = {
+        // mode: 'no-cors',
+        // credentials: 'include',
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            'x-apikey': '3721d83eb3fe9b8df7c29d9f93ec0052da352e49074b7cd9d2283fb944af400f'
+        }
+    };
+
+    fetch(`https://www.virustotal.com/api/v3/urls/${urlEncoded}`, options)
+        .then(response => response.json())
+        .then(function (data) {
+            console.log(data);
+        })
+        .catch(err => console.error(err));
+}
